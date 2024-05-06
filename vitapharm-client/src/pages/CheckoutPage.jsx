cf29b6fb66374c602b666b7ccc265b2c8db85509
agent19music <seanmotanya@gmail.com>
2024-05-06 13:04:09 +0300

    Update checkou page

--- vitapharm-client/src/pages/CheckoutPage.jsx
+++ vitapharm-client/src/pages/CheckoutPage.jsx
@@ -6,7 +6,7 @@ export default function CheckoutPage() {
  
   return (
     <div>
-    
+    <Header/>
         <div class="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
   <a href="#" class="text-2xl font-bold text-gray-800">Vitapharm Checkout</a>
   <div class="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base">
