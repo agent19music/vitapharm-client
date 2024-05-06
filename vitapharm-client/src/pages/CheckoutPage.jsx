b4057475df58bfffaaa7d0c3a6f8ef2c446bcb38
agent19music <seanmotanya@gmail.com>
2024-05-06 12:59:06 +0300

    Add new component header

--- vitapharm-client/src/pages/CheckoutPage.jsx
+++ vitapharm-client/src/pages/CheckoutPage.jsx
@@ -9,41 +9,7 @@ export default function CheckoutPage() {
  
   return (
     <div>
-       {/* <header>
-            <div className='info-bar'>
-                <div className='info-item'>  <TextTransition springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition></div>
-            </div>
-            <div >
-              <div className='primary-bar'>
-                <div className='logo-holder'>  
-                <img src="/seanandprincelogo.jpg" alt="" className='logo' />
-                </div>
-                <div className='search-bar'>
-                   <Input placeholder='Search product or brand' id='search.InputFieldUnderline__input.InputFieldUnderline__input--active' size='md'/>           
-                </div>
-                <div className='whatsapp-info-holder'>
-                  <div className='whatsapp-info'>
-                    <a href="" className='whatsapp'> Whatsapp</a>
-                    <a href="">0745071299</a>
-                  </div>
-                  <div className='free-shipping-banner'>
-                    <h6>Free shipping</h6>
-                    <p>Orders above 3000 bob</p>
-
-                  </div>
-                </div>
-              </div>
-
-            </div>
-            <section className='navbar'>
-        <WithSubnavigation/>
-        <div className='bg-green-700 flex align-middle'>
-         <SideMenu/>
-        </div>
-        
-          
-        </section>
-        </header> */}
+     
         <div class="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
   <a href="#" class="text-2xl font-bold text-gray-800">Vitapharm Checkout</a>
   <div class="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base">
