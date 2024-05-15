8e62aede8a80edcbb3a40451a9273d30b7f4222f
agent19music <seanmotanya@gmail.com>
2024-05-15 16:21:39 +0300

    Improve brands component styling

--- vitapharm-client/src/pages/Brands.jsx
+++ vitapharm-client/src/pages/Brands.jsx
@@ -3,7 +3,7 @@ import brandsWithLetters from '../components/BrandsWithLetters';
 import Header from '../components/Header';
 
 const Brands = () => {
-  let brands = brandsWithLetters
+  let brands = brandsWithLetters;
   const groupedBrands = brands.reduce((acc, brand) => {
     const firstLetter = brand.name.charAt(0).toUpperCase();
     if (!acc[firstLetter]) {
@@ -14,22 +14,24 @@ const Brands = () => {
   }, {});
 
   return (
-   <div>
-    <Header/>
-    <div className="flex flex-wrap justify-center">
-      {Object.entries(groupedBrands).map(([letter, brands]) => (
-        <div key={letter} className="w-full my-4">
-          <h2 className="text-2xl font-bold mb-2">{letter}</h2>
-          <ul className="flex flex-wrap">
-            {brands.map((brand, index) => (
-              <li key={index} className="mx-2">
-                {brand.name}
-              </li>
-            ))}
-          </ul>
+    <div className="">
+      <Header />
+      <div className="container mx-auto py-8">
+        <div className="flex flex-wrap justify-center">
+          {Object.entries(groupedBrands).map(([letter, brands]) => (
+            <div key={letter} className="w-full mb-8">
+              <h2 className="text-3xl font-bold mb-4">{letter}</h2>
+              <ul className="flex flex-wrap">
+                {brands.map((brand, index) => (
+                  <li key={index} className="mx-2 my-1 px-4 py-2 bg-gray-200 rounded-full">
+                    {brand.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
     </div>
   );
 };
