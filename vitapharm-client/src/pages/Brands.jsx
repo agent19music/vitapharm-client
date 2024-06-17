58cb48e15abe96d48a514a50073461df52eed0ec
agent19music <seanmotanya@gmail.com>
2024-06-17 14:38:39 +0300

    Sort the brands alphabetically

--- vitapharm-client/src/pages/Brands.jsx
+++ vitapharm-client/src/pages/Brands.jsx
@@ -1,24 +1,22 @@
-import {React, useContext} from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import { ProductContext } from '../context/ProductContext';
 import { Link } from 'react-router-dom';
 
 const Brands = () => {
-  const {brands} = useContext(ProductContext)
+  const { brands } = useContext(ProductContext);
 
-const extractFirstLetter = (str) => {
-  return str[0].toUpperCase();
-};
+  const extractFirstLetter = (str) => {
+    return str[0].toUpperCase();
+  };
 
-const brandsWithLetters = brands.map((brand) => ({
-  name: brand,
-  letter: extractFirstLetter(brand),
-}));
+  const brandsWithLetters = brands.map((brand) => ({
+    name: brand,
+    letter: extractFirstLetter(brand),
+  }));
 
-
-  
   const groupedBrands = brandsWithLetters.reduce((acc, brand) => {
-    const firstLetter = brand.name.charAt(0).toUpperCase();
+    const firstLetter = brand.letter;
     if (!acc[firstLetter]) {
       acc[firstLetter] = [];
     }
@@ -26,17 +24,29 @@ const brandsWithLetters = brands.map((brand) => ({
     return acc;
   }, {});
 
+  // Sort the letters and brands within each letter group
+  const sortedGroupedBrands = Object.keys(groupedBrands)
+    .sort()
+    .reduce((acc, letter) => {
+      acc[letter] = groupedBrands[letter].sort((a, b) => a.name.localeCompare(b.name));
+      return acc;
+    }, {});
+
   return (
     <div className="">
       <Header />
       <div className="container mx-auto py-8">
         <div className="flex flex-wrap justify-center">
-          {Object.entries(groupedBrands).map(([letter, brands]) => (
+          {Object.entries(sortedGroupedBrands).map(([letter, brands]) => (
             <div key={letter} className="w-full mb-8">
               <h2 className="text-3xl font-bold mb-4">{letter}</h2>
               <ul className="flex flex-wrap">
                 {brands.map((brand, index) => (
-                  <Link to={`/brands/${brand.name}`} key={index} className="mx-2 my-1 px-4 py-2 bg-gray-200 rounded-full">
+                  <Link
+                    to={`/brands/${brand.name}`}
+                    key={index}
+                    className="mx-2 my-1 px-4 py-2 bg-gray-200 rounded-full"
+                  >
                     {brand.name}
                   </Link>
                 ))}
