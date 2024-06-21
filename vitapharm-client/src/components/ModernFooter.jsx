51e56269620b792f35bc5f6197c7c1af9d93fb7a
agent19music <seanmotanya@gmail.com>
2024-06-21 21:41:17 +0300

    Update footer component

--- vitapharm-client/src/components/ModernFooter.jsx
+++ vitapharm-client/src/components/ModernFooter.jsx
@@ -4,18 +4,26 @@ import { Mail, Facebook, Twitter, Instagram, MessageCircle } from 'react-feather
 const Footer = () => {
   return (
     <footer className="bg-brown-custom text-white p-10 font-futura">
+      <div className="flex justify-between items-center mb-10">
+        <div></div>
+        <div className="flex space-x-4">
+          <Facebook className="text-white hover:text-pink-300" />
+          <Twitter className="text-white hover:text-pink-300" />
+          <Instagram className="text-white hover:text-pink-300" />
+          <Mail className="text-white hover:text-pink-300" />
+          <MessageCircle className="text-white hover:text-pink-300" />
+        </div>
+      </div>
       <div className="grid grid-cols-4 gap-10">
         <div>
           <h3 className="font-bold text-lg mb-2">OUR LOCATIONS</h3>
-          <p>Nairobi- Adile Plaza, Chania</p>
-          <p>Nairobi Jammia Mal, CBD Shop</p>
-          <p>Mombasa- Nyoli Centre Shoje</p>
-          <p>Jubileo Exchange Building, Kaunda Stech Shop Na. 3, CBD</p>
+          <p>Nairobi- Adlife Plaza, Chania</p>
+          <p>Nairobi Sasa Mall, CBD Shop</p>
         </div>
         <div>
           <h3 className="font-bold text-lg mb-2">STAY IN THE LOOP</h3>
-          <input className="w-full px-3 py-2 mb-2 border border-gray-300 text-brown-custom rounded-md" type="email" placeholder="Email Address" />
-          <button className="w-full px-3 py-2 bg-white text-brown-custom font-bold rounded-md">Subscribe</button>
+          <input className="w-full px-3 py-2 mb-2 border border-gray-300 text-brown-custom rounded-md focus:outline-none focus:ring-2 focus:ring-brown-custom" type="email" placeholder="Email Address" />
+          <button className="w-full px-3 py-2 bg-white text-brown-custom font-bold hover:border-brown-custom">Subscribe</button>
         </div>
         <div>
           <h3 className="font-bold text-lg mb-2">GET TO KNOW US</h3>
@@ -34,13 +42,7 @@ const Footer = () => {
       </div>
       <div className="flex justify-between items-center mt-10">
         <p>© 2024 - Vitapharm Cosmetics and Pharmacy</p>
-        <div className="flex space-x-4">
-          <Facebook className="text-white hover:text-pink-300" />
-          <Twitter className="text-white hover:text-pink-300" />
-          <Instagram className="text-white hover:text-pink-300" />
-          <Mail className="text-white hover:text-pink-300" />
-          <MessageCircle className="text-white hover:text-pink-300" />
-        </div>
+        <div></div>
       </div>
     </footer>
   );
