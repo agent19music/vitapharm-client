d2324c72dcb07c1a9114e48168122692732d47c8
agent19music <seanmotanya@gmail.com>
2024-06-22 10:40:01 +0300

    Update category component

--- vitapharm-client/src/components/Category.jsx
+++ vitapharm-client/src/components/Category.jsx
@@ -10,7 +10,7 @@ const ProductCard = ({ product, addToCart }) => {
   const price = firstVariation ? firstVariation.price : null;
 
   return (
-    <div className="group border-zinc-100/30 flex w-full max-w-xs flex-col self-center overflow-hidden border bg-zinc-100 shadow-md">
+    <div className="group border-zinc-100/30 flex w-full max-w-xs flex-col self-center overflow-hidden border shadow-md">
    <Link to={`/product/${product.id}`} className="relative mx-3 mt-3 flex h-60 overflow-hidden">  
   <img className="peer absolute top-0 right-0 h-full w-full object-cover" src={`${product.images[0]?.url}`} alt="product image" />
   {product.images.length > 1 && (
@@ -33,7 +33,7 @@ const ProductCard = ({ product, addToCart }) => {
             )} */}
           </p>
         </div>
-        <button onClick={() => addToCart(product.id)} className="hover:border-white/40 flex items-center justify-center border border-transparent vp-bo px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 hover:text-white hover:font-semibold ">
+        <button onClick={() => addToCart(product.id)} className="hover:border-white/40 flex items-center justify-center border border-transparent vp-bo px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 hover:text-white font-futurabold hover:font-semibold ">
           <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
