9d9e8c630c828822513cbe0a1ee10722bcb1316e
agent19music <seanmotanya@gmail.com>
2024-06-29 19:49:49 +0300

    Update the search results display

--- vitapharm-client/src/components/Header.jsx
+++ vitapharm-client/src/components/Header.jsx
@@ -1,20 +1,18 @@
-import React,{useState, useEffect, useContext} from 'react'
-import WithSubnavigation from '../components/Navbar'
-import TextTransition,{presets} from 'react-text-transition'
-import SideMenu from '../components/SideMenu'
-import { Link as RouterLink } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import WithSubnavigation from '../components/Navbar';
+import TextTransition, { presets } from 'react-text-transition';
+import SideMenu from '../components/SideMenu';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { Search } from 'react-feather';
-import WhatsappFloatingActionButton from './WhatsappFloatingActionButton'
-import { InputGroup, Input, InputRightElement, IconButton, Popover, PopoverTrigger, PopoverContent, Box, SimpleGrid, Text, Link, Flex, Image } from '@chakra-ui/react';
-import axios from 'axios';
+import WhatsappFloatingActionButton from './WhatsappFloatingActionButton';
+import { InputGroup, Input, InputRightElement, Popover, PopoverTrigger, PopoverContent, Box, SimpleGrid, Text, Link, Flex, Image, Button } from '@chakra-ui/react';
 import { ProductContext } from '../context/ProductContext';
 
-
 export default function Header() {
-
-  const {products} = useContext(ProductContext)
+  const { products } = useContext(ProductContext);
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
   const TEXTS = [
     '30% OFF ON ALL FACIAL PRODUCTS ON SUNDAYS',
@@ -29,124 +27,144 @@ export default function Header() {
       () => setIndex((index) => index + 1),
       4800 // every 4.8 seconds
     );
-    return () => clearTimeout(intervalId);
+    return () => clearInterval(intervalId);
   }, []);
- 
+
   function handleSearch(query) {
     let lowerCaseQuery = query.toLowerCase();
 
     let filtered = products.filter(product => {
-        let name = product.name.toLowerCase();
-        let description = product.description.toLowerCase();
-        let category = product.category.toLowerCase();
-        let subCategory = product.sub_category.toLowerCase();
+      let name = product.name.toLowerCase();
+      let description = product.description.toLowerCase();
+      let category = product.category.toLowerCase();
+      let subCategory = product.sub_category.toLowerCase();
 
-        return name.includes(lowerCaseQuery) || description.includes(lowerCaseQuery) || category.includes(lowerCaseQuery) || subCategory.includes(lowerCaseQuery);
+      return name.includes(lowerCaseQuery) || description.includes(lowerCaseQuery) || category.includes(lowerCaseQuery) || subCategory.includes(lowerCaseQuery);
     });
 
     setSearchResults(filtered);
-}
-
-const handleChange = (e) => {
-  const query = e.target.value;
-  setSearchQuery(query);
-  if (query.length >= 2) {
-    handleSearch(query);
-  } else {
-    setSearchResults([]); // clear results when query is less than 2 characters
   }
-};
 
-  // Render the categories
-  
+  const handleChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    if (query.length >= 2) {
+      handleSearch(query);
+    } else {
+      setSearchResults([]); // clear results when query is less than 2 characters
+    }
+  };
+
+  const handleViewAll = () => {
+    navigate('/search-results', { state: { results: searchResults } });
+  };
 
   return (
     <header>
-        <div className='info-bar'>
-          <div className='info-item font-futurabold'>
-            <TextTransition springConfig={presets.wobbly}>
-              {TEXTS[index % TEXTS.length]}
+      <div className='info-bar'>
+        <div className='info-item font-futurabold'>
+          {TEXTS.map((text, idx) => (
+            <TextTransition key={idx} springConfig={presets.wobbly}>
+              {TEXTS[(index + idx) % TEXTS.length]}
             </TextTransition>
-          </div>
-        </div>
-        <div className='primary-bar'>
-          <RouterLink className='logo-holder ' to={'/111'}>
-            <img src='/logo.png' alt='' className='logo' />
-          </RouterLink>
-          <div className='search-bar align-bottom min-h-max mx-4'>
-  <Popover isOpen={searchResults.length > 0}>
-    <PopoverTrigger>
-      <InputGroup size="lg">
-        <Input
-          placeholder="Search products or brands"
-          border="2px"
-          borderColor="black.400"
-          focusBorderColor='#693F2D'
-          borderRadius="lg"
-          py="6"
-          pr="12"
-          fontSize="lg"
-          value={searchQuery}
-          onChange={handleChange}
-          className='font-futurabold'
-        />
-        <InputRightElement
-          pointerEvents="none"
-          children={<Search color="gray" />}
-          mr="2"
-        />
-      </InputGroup>
-    </PopoverTrigger>
-    <PopoverContent  width="75vw">
-      <Box p={4} w='100%'  >
-        <SimpleGrid columns={[3, null, 3]} spacing='40px' width='100%'>
-          {searchResults.map((result, index) => (
-            <Link
-              as={RouterLink}
-              to={`/product/${result.id}`}
-              key={index}
-              style={{ textDecoration: 'none' }}
-              className='custom-link font-futura'
-            >
-              <Flex
-                align="center"
-                className='bg-zinc-100 rounded-md hover:bg-brown-custom hover:text-white'
-                style={{ textDecoration: 'none' }}
-              >
-                <Image
-                  src={`${result.images[0]?.url}`}
-                  alt={result.name}
-                  boxSize="50px"
-                  objectFit="cover"
-                  borderRadius="md"
-                  mr={3}
-                />
-                <Text style={{ textDecoration: 'none' }}>{result.name}</Text>
-              </Flex>
-            </Link>
           ))}
-        </SimpleGrid>
-      </Box>
-    </PopoverContent>
-  </Popover>
-</div>
+        </div>
+      </div>
+      <div className='primary-bar'>
+        <RouterLink className='logo-holder ' to={'/111'}>
+          <img src='/logo.png' alt='' className='logo' />
+        </RouterLink>
+        <div className='search-bar align-bottom min-h-max mx-4'>
+          <Popover isOpen={searchResults.length > 0}>
+            <PopoverTrigger>
+              <InputGroup size="lg">
+                <Input
+                  placeholder="Search products or brands"
+                  border="2px"
+                  borderColor="black.400"
+                  focusBorderColor='#693F2D'
+                  borderRadius="lg"
+                  py="6"
+                  pr="12"
+                  fontSize="lg"
+                  value={searchQuery}
+                  onChange={handleChange}
+                  className='font-futurabold'
+                />
+                <InputRightElement
+                  pointerEvents="none"
+                  children={<Search color="gray" />}
+                  mr="2"
+                />
+              </InputGroup>
+            </PopoverTrigger>
+            <PopoverContent width="75vw">
+              <Box p={4} w='100%'>
+                <SimpleGrid columns={[3, null, 3]} spacing='40px' width='100%'>
+                  {searchResults.slice(0, 9).map((result, index) => {
+                    const firstVariation = result.variations?.[0];
+                    const price = firstVariation ? firstVariation.price : null;
+                    const size = firstVariation ? firstVariation.size : null;
 
-          <div className='whatsapp-info-holder '>
-            <div className='whatsapp-info'>
-              <div className=' flex align-middle'>
-                <SideMenu />
-              </div>
-            </div>
-            <div className='free-shipping-banner'>
-              <h6 className='font-futurabold'>Free shipping</h6>
-              <p className='font-futurabold'>Orders above 3000 bob</p>
+                    return (
+                      <Link
+                        as={RouterLink}
+                        to={`/product/${result.id}`}
+                        key={index}
+                        style={{ textDecoration: 'none' }}
+                        className='custom-link font-futura'
+                      >
+                        <Flex
+                          align="center"
+                          justify="space-between"
+                          className='bg-zinc-100 rounded-md hover:bg-brown-custom hover:text-white'
+                          style={{ textDecoration: 'none' }}
+                          p={3}
+                        >
+                          <Flex direction="column">
+                            <Text style={{ textDecoration: 'none' }}>{result.name}</Text>
+                            {price && size && (
+                              <Text>{`KSH ${price} ${size}`}</Text>
+                            )}
+                          </Flex>
+                          <Image
+                            src={`${result.images[0]?.url}`}
+                            alt={result.name}
+                            boxSize="70px"
+                            objectFit="cover"
+                            borderRadius="md"
+                          />
+                        </Flex>
+                      </Link>
+                    );
+                  })}
+                </SimpleGrid>
+                {searchResults.length > 9 && (
+                  <Button mt={4} onClick={handleViewAll} className='font-futurabold align-middle'>
+                    View All Results
+                  </Button>
+                )}
+              </Box>
+            </PopoverContent>
+          </Popover>
+        </div>
+
+        <div className='whatsapp-info-holder '>
+          <div className='whatsapp-info'>
+            <div className=' flex align-middle'>
+              <SideMenu />
             </div>
           </div>
+          <div className='free-shipping-banner'>
+            <h6 className='font-futurabold'>Free shipping</h6>
+            <p className='font-futurabold'>Orders above 3000 bob</p>
+          </div>
         </div>
-        <section className='navbar'>
-          <WithSubnavigation/>
-        </section>
-        <WhatsappFloatingActionButton/>
-      </header>
-  )
+      </div>
+      <section className='navbar'>
+        <WithSubnavigation />
+      </section>
+      <WhatsappFloatingActionButton />
+    </header>
+  );
 }
