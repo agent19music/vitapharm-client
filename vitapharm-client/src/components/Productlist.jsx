87aa2c9889815cf0a84b672f7f5df62a9ff57faf
agent19music <seanmotanya@gmail.com>
2024-06-30 18:07:17 +0300

    Update the sidemenu

--- vitapharm-client/src/components/Productlist.jsx
+++ vitapharm-client/src/components/Productlist.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({ product, addToCart }) => {
 
   return (
     <div className="group border-zinc-100/30 flex w-full max-w-xs flex-col self-center overflow-hidden border shadow-md product-card">
-      <Link to={`/product/${product.id}`} className="relative mx-3 mt-3 flex h-80 overflow-hidden">  
+      <Link className="relative mx-3 mt-3 flex h-80 overflow-hidden">  
         <img className="peer absolute top-0 right-0 h-full w-full object-cover" src={`${product.images[0]?.url}`} alt="product image" />
         {product.images.length > 1 && (
     <>
--- vitapharm-client/src/context/ProductContext.jsx
+++ vitapharm-client/src/context/ProductContext.jsx
@@ -1,148 +1,141 @@
 import { createContext, useState, useEffect } from 'react';
 import { useToast } from "@chakra-ui/react";
 
-
 export const ProductContext = createContext();
 
 export default function ProductProvider({ children }) {
+  const apiEndpoint = 'http://server-env.eba-8hpawwgj.eu-north-1.elasticbeanstalk.com/api/vitapharm';
 
-  
+  const [products, setProducts] = useState([]);
+  const [filteredCategories, setFilteredCategories] = useState([]);
+  const [filteredBrands, setFiltredBrands] = useState([]);
+  const [sessionToken, setSessionToken] = useState(null);
+  const [updateCart, setUpdateCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [onChange, setOnChange] = useState(false);
+  const [subtotal, setSubtotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [cartItemCount, setCartItemCount] = useState(0);
+  const [cartEmpty, setCartEmpty] = useState(true);
+  const [category, setCategory] = useState('');
+  const [brand, setBrand] = useState('');
 
-    const apiEndpoint = 'http://server-env.eba-8hpawwgj.eu-north-1.elasticbeanstalk.com/api/vitapharm';
+  const toast = useToast();
 
-    const [products, setProducts] = useState([]);
-    const [filteredCategories, setFilteredCategories] = useState([]);
-    const [filteredBrands,setFiltredBrands]= useState([])
-    const [sessionToken, setSessionToken] = useState(null);
-    const [updateCart, setUpdateCart] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [onChange, setOnChange] = useState(false);
-    const [subtotal, setSubtotal] = useState(0);
-    const [total, setTotal] = useState(0);
-    const [cartItemCount, setCartItemCount] = useState(0); // State to track number of items in cart
-    const [cartEmpty, setCartEmpty] = useState(true); // State to track if cart is empty
-    const [category, setCategory] = useState('');
-    const [brand, setBrand] = useState('');
+  useEffect(() => {
+    const fetchSessionToken = async () => {
+      try {
+        let storedToken = localStorage.getItem('session_token');
+        let tokenExpiration = localStorage.getItem('token_expiration');
 
-    const toast = useToast();
-
-
-
-
-    useEffect(() => {
-      const fetchSessionToken = async () => {
-          try {
-              let storedToken = localStorage.getItem('session_token');
-              let tokenExpiration = localStorage.getItem('token_expiration');
-  
-              if (storedToken && tokenExpiration && new Date(tokenExpiration) > new Date()) {
-                  setSessionToken(storedToken);
-              } else {
-                  const response = await fetch(`${apiEndpoint}/session`);
-                  const { session_token } = await response.json();
-                  const expiration = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
-                  localStorage.setItem('session_token', session_token);
-                  localStorage.setItem('token_expiration', expiration);
-                  setSessionToken(session_token);
-              }
-          } catch (error) {
-              console.error('Error fetching session token:', error);
-          }
-      };
-  
-      fetchSessionToken(); // Fetch token on component mount
-  
-      // expiration timer for the token
-      const refreshInterval = setInterval(() => {
-          fetchSessionToken();
-      }, 55 * 60 * 1000); // 55 minutes
-  
-      return () => clearInterval(refreshInterval);
-  }, []);
-  
-
-
-    useEffect(() => {
-        const fetchData = async (token) => {
-            try {
-                const response = await fetch(`${apiEndpoint}/products`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                         'Authorization': `Bearer ${token}`
-                    },
-                });
-                const data = await response.json();
-                setProducts(data);
-                setFilteredCategories([]);
-                setFiltredBrands([]);
-                // console.log("Data fetched:", data); // Log the data here
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        
-        if (sessionToken) {
-            fetchData(sessionToken);
+        if (storedToken && tokenExpiration && new Date(tokenExpiration) > new Date()) {
+          setSessionToken(storedToken);
+        } else {
+          const response = await fetch(`${apiEndpoint}/session`);
+          const { session_token } = await response.json();
+          const expiration = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
+          localStorage.setItem('session_token', session_token);
+          localStorage.setItem('token_expiration', expiration);
+          setSessionToken(session_token);
         }
-    
-    }, [sessionToken]);
-
-    
-
-    const addToCart = async (id) => {
-        if (!sessionToken) return; // Handle missing token
-    
-        try {
-          const response = await fetch(`${apiEndpoint}/cart/add`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${sessionToken}`
-            },
-            body: JSON.stringify({ product_id: id, quantity: 1 })
-          });
-          const data = await response.json();
-          // console.log(data);
-    
-          // Add the toast here
-          toast({
-            title: "Product added.",
-            description: "The product has been added to your cart.",
-            status: "success",
-            duration: 1200,
-            isClosable: true,
-            position: "top-right",
-            variant: "subtle",
-            colorScheme: "green",
-          });
-
-          setUpdateCart(prev => !prev); // Trigger cart update
-    
-        } catch (error) {
-          console.error('Error adding to cart:', error);
-        }
-        // setUpdateCart(true); // Trigger cart update
-        // setUpdateCart(false); // Reset trigger
-      };
-
-      const calculateCartTotal = (cartData) => {
-        if (!Array.isArray(cartData)) {
-            console.error('calculateCartTotal expected an array, but received:', cartData);
-            return;
-        }
-        
-        let subtotalPrice = 0;
-        cartData.forEach((item) => {
-            subtotalPrice += item.quantity * item.total_price; // Ensure correct calculation
-        });
-        setSubtotal(subtotalPrice);
-        setTotal(subtotalPrice);
+      } catch (error) {
+        console.error('Error fetching session token:', error);
+      }
     };
+
+    fetchSessionToken(); // Fetch token on component mount
+
+    const refreshInterval = setInterval(() => {
+      fetchSessionToken();
+    }, 55 * 60 * 1000); // 55 minutes
+
+    return () => clearInterval(refreshInterval);
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async (token) => {
+      try {
+        const response = await fetch(`${apiEndpoint}/products`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        });
+        const data = await response.json();
+        setProducts(data);
+        setFilteredCategories([]);
+        setFiltredBrands([]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (sessionToken) {
+      fetchData(sessionToken);
+    }
+  }, [sessionToken]);
+
+  const addToCart = async (id) => {
+    if (!sessionToken) return; // Handle missing token
     
+    const isProductInCart = cartItems.some(item => item.product_id === id);
+    if (isProductInCart) {
+      toast({
+        title: "Product already in cart.",
+        description: "This product is already in your cart.",
+        status: "info",
+        duration: 1200,
+        isClosable: true,
+        position: "top-right",
+        variant: "subtle",
+        colorScheme: "yellow",
+      });
+      return;
+    }
+
+    try {
+      const response = await fetch(`${apiEndpoint}/cart/add`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${sessionToken}`
+        },
+        body: JSON.stringify({ product_id: id, quantity: 1 })
+      });
+      const data = await response.json();
       
+      toast({
+        title: "Product added.",
+        description: "The product has been added to your cart.",
+        status: "success",
+        duration: 1200,
+        isClosable: true,
+        position: "top-right",
+        variant: "subtle",
+        colorScheme: "green",
+      });
+
+      setUpdateCart(prev => !prev); // Trigger cart update
+    } catch (error) {
+      console.error('Error adding to cart:', error);
+    }
+  };
+
+  const calculateCartTotal = (cartData) => {
+    if (!Array.isArray(cartData)) {
+      console.error('calculateCartTotal expected an array, but received:', cartData);
+      return;
+    }
     
-  
+    let subtotalPrice = 0;
+    cartData.forEach((item) => {
+      subtotalPrice += item.quantity * item.total_price;
+    });
+    setSubtotal(subtotalPrice);
+    setTotal(subtotalPrice);
+  };
+
   useEffect(() => {
     const fetchCartItems = async () => {
       if (!sessionToken) return;
@@ -163,8 +156,8 @@ export default function ProductProvider({ children }) {
         const data = await response.json();
         setCartItems(data);
         calculateCartTotal(data);
-        setCartItemCount(data.length); // Update cart item count
-        setCartEmpty(data.length === 0); // Update cart empty status
+        setCartItemCount(data.length);
+        setCartEmpty(data.length === 0);
       } catch (error) {
         console.error('Error fetching cart items:', error);
       }
@@ -176,89 +169,94 @@ export default function ProductProvider({ children }) {
   const updateCartItemQuantity = async (productId, quantityChange) => {
     if (!sessionToken) return;
     try {
-        // Optimistically update the cart items in the state
-        setCartItems(prevCartItems => {
-            const updatedCartItems = prevCartItems.map(item => 
-                item.product_id === productId ? { ...item, quantity: item.quantity + quantityChange } : item
-            ).filter(item => item.quantity > 0); // Remove items with quantity <= 0
+      setCartItems(prevCartItems => {
+        const updatedCartItems = prevCartItems.map(item => 
+          item.product_id === productId ? { ...item, quantity: item.quantity + quantityChange } : item
+        ).filter(item => item.quantity > 0);
 
-            calculateCartTotal(updatedCartItems); // Update totals
-            return updatedCartItems;
-        });
+        calculateCartTotal(updatedCartItems);
+        return updatedCartItems;
+      });
 
-        // Send the update request to the server
-        const response = await fetch(`${apiEndpoint}/cart/update`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${sessionToken}`
-            },
-            body: JSON.stringify({
-                product_id: productId,
-                quantity_change: quantityChange
-            })
-        });
+      const response = await fetch(`${apiEndpoint}/cart/update`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${sessionToken}`
+        },
+        body: JSON.stringify({
+          product_id: productId,
+          quantity_change: quantityChange
+        })
+      });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            console.error(`Error ${response.status}: ${response.statusText}`, errorData);
-            // Revert the optimistic update in case of error
-            setUpdateCart(prev => !prev);
-        } else {
-            const updatedCart = await response.json();
-            // setCartItems(updatedCart)
-            console.log('Updated Cart Response:', updatedCart);
-            // You can optionally set the state again here based on the server response
-        }
-    } catch (error) {
-        console.error('Error updating cart item quantity:', error);
-        // Revert the optimistic update in case of error
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error(`Error ${response.status}: ${response.statusText}`, errorData);
         setUpdateCart(prev => !prev);
+      } else {
+        const updatedCart = await response.json();
+        console.log('Updated Cart Response:', updatedCart);
+      }
+    } catch (error) {
+      console.error('Error updating cart item quantity:', error);
+      setUpdateCart(prev => !prev);
     }
-};
+  };
 
   const removeCartItem = async (productId) => {
     try {
-        const response = await fetch(`${apiEndpoint}/cart/remove`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${sessionToken}`
-            },
-            body: JSON.stringify({
-                product_id: productId
-            })
-        });
+      const response = await fetch(`${apiEndpoint}/cart/remove`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${sessionToken}`
+        },
+        body: JSON.stringify({
+          product_id: productId
+        })
+      });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            console.error(`Error ${response.status}: ${response.statusText}`, errorData);
-            // Handle error as needed
-        } else {
-            // Item removed successfully, update cart
-            setUpdateCart(prev => !prev);
-            // Optionally show a success message
-            toast({
-                title: "Item removed from cart.",
-                status: "success",
-                duration: 1200,
-                isClosable: true,
-                position: "top-right",
-                variant: "subtle",
-                colorScheme: "green",
-            });
-        }
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error(`Error ${response.status}: ${response.statusText}`, errorData);
+      } else {
+        setUpdateCart(prev => !prev);
+        toast({
+          title: "Item removed from cart.",
+          status: "success",
+          duration: 1200,
+          isClosable: true,
+          position: "top-right",
+          variant: "subtle",
+          colorScheme: "green",
+        });
+      }
     } catch (error) {
-        console.error('Error removing cart item:', error);
+      console.error('Error removing cart item:', error);
     }
   };
-  
+
   const incrementQuantity = (productId) => {
     updateCartItemQuantity(productId, 1);
   };
 
   const decrementQuantity = (productId) => {
-    updateCartItemQuantity(productId, -1);
+    const item = cartItems.find(item => item.product_id === productId);
+    if (item && item.quantity > 1) {
+      updateCartItemQuantity(productId, -1);
+    } else {
+      toast({
+        title: "Minimum quantity reached.",
+        description: "You cannot reduce the quantity below 1.",
+        status: "info",
+        duration: 1200,
+        isClosable: true,
+        position: "top-right",
+        variant: "subtle",
+        colorScheme: "yellow",
+      });
+    }
   };
 
   function extractBrands(data) {
@@ -267,12 +265,12 @@ export default function ProductProvider({ children }) {
   }
 
   function extractCategories(data) {
-    const brands = data.map(item => item.category);
-    return [...new Set(brands)];
+    const categories = data.map(item => item.category);
+    return [...new Set(categories)];
   }
-  let categories =extractCategories(products)
-  
-  let brands = extractBrands(products)
+
+  let categories = extractCategories(products);
+  let brands = extractBrands(products);
 
   const extractFirstLetter = (str) => {
     return str[0].toUpperCase();
@@ -282,7 +280,6 @@ export default function ProductProvider({ children }) {
     name: brand,
     letter: extractFirstLetter(brand),
   }));
- 
 
   useEffect(() => {
     if (category) {
@@ -303,7 +300,6 @@ export default function ProductProvider({ children }) {
   }, [brand, products]);
 
   const contextData = {
-  
     products,
     apiEndpoint,
     addToCart,
@@ -326,15 +322,11 @@ export default function ProductProvider({ children }) {
     setCategory,
     setBrand,
     filteredBrands,
-    
-    
-     
-};
+  };
 
-
-    return (
-        <ProductContext.Provider value={contextData}>
-            {children}
-        </ProductContext.Provider>
-    );
+  return (
+    <ProductContext.Provider value={contextData}>
+      {children}
+    </ProductContext.Provider>
+  );
 }
