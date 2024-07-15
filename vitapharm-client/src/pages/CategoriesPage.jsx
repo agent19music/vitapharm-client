374e4a6f23cee9014225f795cf817f22e89ed6bd
agent19music <seanmotanya@gmail.com>
2024-07-15 11:17:11 +0300

    Update categories list page

--- vitapharm-client/src/pages/CategoriesPage.jsx
+++ vitapharm-client/src/pages/CategoriesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ProductContext } from '../context/ProductContext';
 import { Link } from 'react-router-dom';
 import { Box, VStack, Skeleton, SkeletonCircle, Heading, Text } from '@chakra-ui/react';
@@ -7,6 +7,18 @@ import Footer from '../components/ModernFooter';
 
 const CategoriesPage = () => {
   const { subCategories } = useContext(ProductContext);
+  const [sortedSubCategories, setSortedSubCategories] = useState([]);
+
+  useEffect(() => {
+    if (subCategories) {
+      const sorted = [...subCategories].sort((a, b) => a.category.localeCompare(b.category));
+      sorted.forEach(category => {
+        category.sub_categories.sort();
+        category.sub_categories = category.sub_categories.map(subCategory => subCategory.charAt(0).toUpperCase() + subCategory.slice(1));
+      });
+      setSortedSubCategories(sorted);
+    }
+  }, [subCategories]);
 
   const skeletonLoading = (
     <VStack spacing={8} align="center">
@@ -21,7 +33,7 @@ const CategoriesPage = () => {
     </VStack>
   );
 
-  if (!subCategories || subCategories.length === 0) {
+  if (!sortedSubCategories || sortedSubCategories.length === 0) {
     // Render skeleton loading while data is being fetched
     return (
       <>
@@ -39,14 +51,16 @@ const CategoriesPage = () => {
       <Header />
       <Box p={8}>
         <VStack spacing={8} align="start">
-          {subCategories.map((category, index) => (
+          {sortedSubCategories.map((category, index) => (
             <Box key={index} width="100%">
+                <Link  to={`/categories/${category.category}`} className='hover:text-brown-custom'>
               <h2 className="text-3xl font-bold font-futuramedbold mb-4">{category.category.toUpperCase()}</h2>
+              </Link>
             
               <ul className="flex flex-wrap">
                 {category.sub_categories.map((subCategory, subIndex) => (
                   <Link
-                    to={`/category/${category.category}/${subCategory}`}
+                    to={`/categories/${category.category}/${subCategory}`}
                     key={subIndex}
                     className="mx-2 my-1 px-4 py-2 bg-brown-custom text-white hover:text-white rounded-full font-futurabold"
                   >
