88992e3b4071add8e5fba0c2469f7ff90d02049b
agent19music <seanmotanya@gmail.com>
2024-07-28 20:48:10 +0300

    Return Ksh before price

--- vitapharm-client/src/components/RecentlyAddedProducts.jsx
+++ vitapharm-client/src/components/RecentlyAddedProducts.jsx
@@ -44,7 +44,7 @@ const ProductCard = ({ product, addToCart }) => {
         </a>
         <div className="mt-2 mb-5 flex items-center justify-between">
           <p>
-            <span className="text-3xl font-futura font-bold text-black"> {price !== null ? price : "N/A"}</span>
+            <span className="text-3xl font-futura font-bold text-black">Ksh {price !== null ? price : "N/A"}</span>
             <span className="text-1.5xl font-futura font-bold text-black ml-3" style={{ lineHeight: '0.5' }}>{size !== null ? size.toUpperCase() : "N/A"}</span>
           </p>
         </div>
