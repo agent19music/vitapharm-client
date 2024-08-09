27f4356295ae8842c9400851c6bfc6a6ce2fbca5
agent19music <seanmotanya@gmail.com>
2024-08-09 13:17:03 +0300

    Update free shipping banner

--- vitapharm-client/src/components/Header.jsx
+++ vitapharm-client/src/components/Header.jsx
@@ -268,7 +268,7 @@ export default function Header() {
           </div>
           <div className='free-shipping-banner'>
             <h6 className='font-futurabold'>Free shipping</h6>
-            <p className='font-futurabold'>Orders above 8000 bob</p>
+            <p className='font-futurabold'>Orders above 15,000 bob</p>
           </div>
         </div>
       </div>
